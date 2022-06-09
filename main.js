78cb73a6c6bd57753a66cedf7f3b68b98867a54f
Dan Tasse <dan.tasse@gmail.com>
2022-06-08 22:58:27 -0700

    ok we have some cubes

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import { Scene } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
 console.log('foo')
